0e6cd5c8a9bad87f6daa67efe25af2cf53e458ca
MANU PRATAP SINGH <90621084+manu9458@users.noreply.github.com>
2022-02-12 17:38:14 +0530

    Update cquizgame.c

--- 3_Implementation/src/cquizgame.c
+++ 3_Implementation/src/cquizgame.c
@@ -21,7 +21,6 @@ int main()
      printf("\n\t\t\t   THE GAME ");
      printf("\n\t\t________________________________________");
      printf("\n\t\t________________________________________");
-     printf("\n\t\t   BECOME A MILLIONAIRE!!!!!!!!!!!    ") ;
      printf("\n\t\t________________________________________");
      printf("\n\t\t________________________________________");
      printf("\n\t\t > Press S to start the game");
@@ -485,17 +484,6 @@ void help()
     printf("\n\n                              HELP");
     printf("\n -------------------------------------------------------------------------");
     printf("\n ......................... C program Quiz Game...........");
-    printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
-    printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
-    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
-    printf("\n    right answers otherwise you can't play the Game...........");
-    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
-    printf("\n    total 10 questions each right answer will be awarded $100,000.");
-    printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
-    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
-    printf("\n    right option");
-    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
-    printf("\n >> No negative marking for wrong answers");
 
 }
 
